434edebbfe2a2c7296534bae2aefb27d14672efe
castynet <riungemaina@gmail.com>
2021-01-03 15:04:12 +0300

    added production code

---
+++ precache-manifest.e58695ab4d281db73f95ed08b52dd331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae8d6912a86cbc9bfd3d039d5850a68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "514a095e416634608a53",
+    "url": "/static/css/main.dea2b19e.chunk.css"
+  },
+  {
+    "revision": "bea988f2dcd846a0c688",
+    "url": "/static/js/2.0590f04f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0590f04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514a095e416634608a53",
+    "url": "/static/js/main.c0ea493f.chunk.js"
+  },
+  {
+    "revision": "7e55085a18130c3fb6eb",
+    "url": "/static/js/runtime-main.b815fcf4.js"
+  }
+]);
